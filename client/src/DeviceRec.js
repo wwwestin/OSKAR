d2678817579c35962744c03c497c485053418b0f
Westin Humble <westin.h@gmail.com>
2022-05-04 15:37:09 -0400

    refined device rec

--- client/src/DeviceRec.js
+++ client/src/DeviceRec.js
@@ -16,7 +16,7 @@ function DeviceRec ({currentPatient, currentDevice}) {
         fetch(`/devices/${id}`)
           .then((r) => r.json())
           .then(data => setShowCurrentDevice(data));
-      }, {});
+      }, []);
     
     return (
         <div className= "deviceRec">
