3466a89e8d8d70d4a21c4cac8139f5962451d914
Westin Humble <westin.h@gmail.com>
2022-05-07 15:51:48 -0400

    first version of header and login page styled

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,3 +1,7 @@
+import React from "react";
+import "./App.css";
+import {Container, Row, Col, Navbar, Button} from "react-bootstrap"
+
 function Header ({user,onLogout}){
     
     function handleLogout() {
@@ -7,30 +11,55 @@ function Header ({user,onLogout}){
         }
     
     return (
-        <div class="headerContainer">
-              <h1 class= "title">OSKAR</h1>
-                {user ? (
-                    <div class= "profile">
+        <div>
+            <Container>
+            <Row>
+            <Col></Col>
+                <Col style={{display:'flex', justifyContent:'center'}}>
+                    <h1><b>OSKAR</b></h1>
+                </Col>
+                <Col style={{display:'flex', justifyContent:'right', paddingTop: 10}}>
+                        {user ? (
+                        <div class= "profile">
                         <p>Welcome, {user.username}!</p>
                         <br></br>
-                        <button class= "logoutButton" onClick={handleLogout}>Logout</button>
+                        <Button class= "logoutButton" onClick={handleLogout}>Logout</Button>
                     </div>
-                ) : (
+                    ) : (
                     <div>
-                        <a id="loginButton"href="/">Login</a>
+                        <Button><a className = "buttonText" href="/">Login</a></Button>
                     </div>
-                )}
-            <div class="containerNav">
-              <nav>
-                  <ul>
-                      <li><a href="/">Home</a></li>
-                      <li><a href="/CreatePatient">Add Patient</a></li>
-                      <li><a href="/EditProfile">Edit Profile</a></li>
-                      <li><a href="/SavedDevices">Saved Devices</a></li>
-                  </ul>
-              </nav>
-          </div>
-        </div>
+                    )}
+                </Col>
+            </Row>
+            <Row>
+                <Col style={{display:'flex', justifyContent:'center'}}>
+                    <p><b>O</b>ptimal <b>S</b>egmental <b>K</b>inematics and <b>A</b>lignment Approach to <b>R</b>ehabilitation</p>
+                </Col>
+            </Row>
+                <div>
+                    <Row>
+                    <Col style={{display:'flex', justifyContent:'center'}}>
+                        <Navbar className="navBar" bg = "dark" variant="light">
+                        <Navbar.Brand href="/">
+                            <img
+                                alt="Person Walking"
+                                src="https://iconsplace.com/wp-content/uploads/_icons/ffffff/256/png/walking-icon-18-256.png"
+                                width="30"
+                                height="30"
+                                className="d-inline-block align-top"
+                            />{' '}
+                                <a className = "buttonText" href="/">Home</a>
+                                <a className = "buttonText" href="/CreatePatient">Add Patient</a>
+                                <a className = "buttonText" href="/EditProfile">Edit Profile</a>
+                                <a className = "buttonText" href="/SavedDevices">Saved Devices</a>
+                            </Navbar.Brand>
+                        </Navbar>
+                    </Col>
+                    </Row>
+            </div>
+        </Container>
+    </div>
     )
   }
   
--- client/src/Login.js
+++ client/src/Login.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import {Link} from "react-router-dom"
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import {Button} from 'react-bootstrap';
+import "./App.css"
 
 
 function Login ({onLogin}) {
@@ -35,46 +36,40 @@ function Login ({onLogin}) {
     })
 }
 
-    return (
-        <Container fluid = "lg">
-        <Row>
-    <Col>
-     <form onSubmit={handleSubmit} className="container">
+return (
+      <form onSubmit={handleSubmit} className="loginContainer">
         <label for="username"><b>Username</b> </label>
             <br></br>
             <input type="text" 
                 id="username" 
-                placeholder="username" 
+                placeholder="Username" 
                 name="username"
                 className="inputLogin"
                 required value={username}
                 onChange={e => setUsername(e.target.value) }
             />
-        <p className="error">{error}</p>
+                <p className="error">{error}</p>
                 <br></br>
             <label for="password"> <b>Password</b> </label>
                 <br></br>
                 <input 
                     type="password" 
-                    placeholder="password" 
+                    placeholder="Password" 
                     name="password" 
                     className="inputLogin"
                     required id="password"
                     value={password}
                     onChange={ e=> setPassword(e.target.value)}
                 />  
-        <p className="error"> {pass}</p>
+                <p className="error"> {pass}</p>
                 <br></br>
             <div className="buttonSection">
-                <Button className= "loginButtons" type="submit"> 
+                <Button bg = "info" className= "loginButtons" type="submit"> 
                     {loading? <Link to="/CreatePatient"> "Loading..."</Link>: "Login" }
                 </Button>
-            <Link to="/CreateProfile"> <Button className= "loginButtons">Create Profile</Button> </Link>
+            <Link to="/CreateProfile"> <Button bg = "info" className= "loginButtons">Create Profile</Button> </Link>
         </div>
     </form>
-    </Col>
-    </Row>
-    </Container>
     )
 }
 
